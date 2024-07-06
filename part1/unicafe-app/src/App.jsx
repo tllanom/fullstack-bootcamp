27fe91f961698757f08f9cb57c5fb97dcc5b76f3
tllanom <135003286+tllanom@users.noreply.github.com>
2024-07-06 20:27:15 +0200

    unicafe 1.6 basic feedback
    give feedback
    
    <button><button><button>
    
    statistics
    
    good 5
    
    neutral 6
    
    bad 6

--- part1/unicafe-app/src/App.jsx
+++ part1/unicafe-app/src/App.jsx
@@ -8,7 +8,14 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <h2>give feedback</h2>
+      <button onClick={() => (setGood(good + 1))}>good</button>
+      <button onClick={() => (setNeutral(neutral + 1))}>neutral</button>
+      <button onClick={() => (setBad(bad + 1))}>bad</button>
+      <h2>statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
   )
 }
