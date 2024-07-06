0a23726706382b75013e5b86fd0692f92d4cbb43
tllanom <135003286+tllanom@users.noreply.github.com>
2024-07-06 21:20:14 +0200

    1.8 - statistics component
    refactoring the application so that displaying the statistics is extracted into its own Statistics component. The state of the application should remain in the App root component.

--- part1/unicafe-app/src/App.jsx
+++ part1/unicafe-app/src/App.jsx
@@ -1,5 +1,23 @@
 import { useState } from 'react'
 
+const Statistics = (props) => {
+  const {good, neutral, bad} = props
+  const all = good + neutral + bad
+  const avg = ((good-bad)/all)
+  const positive = (good/all)*100
+
+  return (
+    <div>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>avg {avg}</p>
+      <p>positive {positive}%</p>
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -13,12 +31,7 @@ const App = () => {
       <button onClick={() => (setNeutral(neutral + 1))}>neutral</button>
       <button onClick={() => (setBad(bad + 1))}>bad</button>
       <h2>statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {(good+neutral+bad)}</p>
-      <p>average {(((good)+(bad*(-1)))/(good+neutral+bad))}</p>
-      <p>positive {(((good)/(good+neutral+bad))*100)}%</p>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
